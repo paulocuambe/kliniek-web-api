c72500a2e770c0f31e3b7e5d8f452afb323c8a2b
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2019-10-10 22:08:07 +0200

    CRUD dos tipos de consultas e exames

---
+++ Kliniek/src/main/java/com/kliniek/api/model/TipoConsulta.java
@@ -0,0 +1,63 @@
+package com.kliniek.api.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TipoConsulta {
+    private long tipoid;
+    private String designacao;
+    private String descricao;
+    private double preco;
+    private boolean disponivel;
+
+    public TipoConsulta() {
+    }
+
+    public TipoConsulta(long tipoid, String designacao, String descricao, double preco, boolean disponivel) {
+        this.tipoid = tipoid;
+        this.designacao = designacao;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.disponivel = disponivel;
+    }
+
+    public long getTipoid() {
+        return tipoid;
+    }
+
+    public void setTipoid(long tipoid) {
+        this.tipoid = tipoid;
+    }
+
+    public String getDesignacao() {
+        return designacao;
+    }
+
+    public void setDesignacao(String designacao) {
+        this.designacao = designacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
